2939db371a3f907effce31c38fc7b94ea108dfe0
Snow <35224538+KibbeWater@users.noreply.github.com>
2022-07-31 06:03:13 +0200

    Added custom titlebar

---
+++ src/components/titlebar.tsx
@@ -0,0 +1,9 @@
+import '../styles/titlebar.css';
+
+export default function Titlebar() {
+	return (
+		<div className='titlebar'>
+			<div className='titlebar__btn__close' />
+		</div>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
+import { motion } from 'framer-motion';
+import Titlebar from '../components/titlebar';
+
+import Link from '../private/link';
+import '../styles/index.css';
+
 export default function Page() {
 	return (
 		<>
-			<div>Hello, World!</div>
+			<Titlebar />
+			<div className='parent parent-window'>
+				<div className='container'>
+					<div>
+						<h1 className='container__text'>Welcome!</h1>
+						<h1 className='container__subtext'>Please sign in to continue</h1>
+					</div>
+					<div>
+						<Link href='/login'>
+							<motion.a
+								className='container__button'
+								whileHover={{
+									y: -4,
+								}}
+							>
+								Login
+							</motion.a>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
